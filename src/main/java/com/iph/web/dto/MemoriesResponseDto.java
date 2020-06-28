df668093c30606b9a3123e99052439b1a2c854a9
aqntks <42826598+aqntks@users.noreply.github.com>
2020-06-28 15:35:43 +0900

    Memories DTO 작성

---
+++ src/main/java/com/iph/web/dto/MemoriesResponseDto.java
@@ -0,0 +1,21 @@
+package com.iph.web.dto;
+
+import com.iph.domain.posts.Memories;
+import lombok.Getter;
+
+@Getter
+public class MemoriesResponseDto {
+
+    private Long id;
+    private String date;
+    private String place;
+    private String content;
+
+    public MemoriesResponseDto(Memories entity){
+        this.id = entity.getId();
+        this.date = entity.getDate();
+        this.place = entity.getPlace();
+        this.content = entity.getContent();
+
+    }
+}
