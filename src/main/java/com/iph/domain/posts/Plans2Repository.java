3628b223fad1b20a3757cf4cb127bd4346ba35ba
aqntks <cjd0030@gmail.com>
2020-07-19 04:19:52 +0900

    오타수정

--- src/main/java/com/iph/domain/posts/Plans2Repository.java
+++ src/main/java/com/iph/domain/posts/Plans2Repository.java
@@ -7,10 +7,10 @@ import java.util.List;
 
 public interface Plans2Repository extends JpaRepository<Plans2, Long>{
 
-    @Query("SELECT p FROM Plans2 p ORDER BY p.id DESC")
+    @Query("SELECT p FROM plans2 p ORDER BY p.id DESC")
     List<Plans2> findAllDesc();
 
-    @Query(value = "SELECT * FROM Plans2 p WHERE p.year = DATE_FORMAT(curdate() , '%Y') " +
+    @Query(value = "SELECT * FROM plans2 p WHERE p.year = DATE_FORMAT(curdate() , '%Y') " +
             "AND p.month = DATE_FORMAT(curdate() , '%c') AND p.date = DATE_FORMAT(curdate() , '%d')", nativeQuery = true)
     List<Plans2> findTodayPlan();
 }
