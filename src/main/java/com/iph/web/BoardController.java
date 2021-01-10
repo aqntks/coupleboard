2f52b1e0204e5f1950dd15b6a5e3d3457a0a14f7
aqntks <cjd0030@gmail.com>
2021-01-10 18:10:57 +0900

    오늘 일정 / 다가오는 일정 출력

--- src/main/java/com/iph/web/BoardController.java
+++ src/main/java/com/iph/web/BoardController.java
@@ -4,7 +4,6 @@ import com.iph.domain.posts.CoupleProfile;
 import com.iph.service.posts.CoupleProfileService;
 import com.iph.service.posts.MemoriesService;
 import com.iph.service.posts.Plans2Service;
-import com.iph.web.dto.CoupleProfileResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -40,14 +39,16 @@ public class BoardController {
             model.addAttribute("couple_profile", entity);
         }
 
-        model.addAttribute("plans2", plans2Service.findTodayPlan());
+        model.addAttribute("today", plans2Service.findTodayPlan());
+        model.addAttribute("next", plans2Service.findNextPlan());
 
         return "home";
     }
 
     @GetMapping("/plan")
     public String plan(Model model) {
-        model.addAttribute("plans2", plans2Service.findTodayPlan());
+        model.addAttribute("today", plans2Service.findTodayPlan());
+        model.addAttribute("next", plans2Service.findNextPlan());
         return "plan";
     }
 
